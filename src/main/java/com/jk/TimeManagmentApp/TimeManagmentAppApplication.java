65bf89ed9874d6ce87715b9c3e4f01e59c376fc1
krzjoz <krz_joz@windowslive.com>
2020-08-12 17:41:52 +0200

    Add Spring

---
+++ src/main/java/com/jk/TimeManagmentApp/TimeManagmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.jk.TimeManagmentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeManagmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeManagmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jk/TimeManagmentApp/TimeManagmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jk.TimeManagmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeManagmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
